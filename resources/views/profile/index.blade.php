0eec591c0fe01ab44efc1a489baccb34814cc143
mehrshad darvish <civilendarvish@gmail.com>
2020-06-10 01:08:34 +0430

    profile front

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -1,108 +1,122 @@
 @extends('master')
 @section('content')
-
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1>Profile</h1>
-                    </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">User Profile</li>
-                        </ol>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-10">
+                    <h1 class="h3 mb-0 text-gray-800 text-capitalize">Welcome {{Auth::user()->name}}</h1>
+                </div>
+                <div>
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4 ml-5">
+                        <a href="#" class = "btn btn-primary p-2 m-2">Edit Profile</a>
                     </div>
                 </div>
-            </div><!-- /.container-fluid -->
-        </section>
+            </div>
+            @if ($message = Session::get('success'))
+                <div class="alert alert-success alert-block">
+                    <button type="button" class="close" data-dismiss="alert">×</button>
+                    <strong>{{ $message }}</strong>
+                </div>
+            @endif
+            @if ($message = Session::get('attention'))
+                <div class="alert alert-danger alert-block">
+                    <button type="button" class="close" data-dismiss="alert">×</button>
+                    <strong>{{ $message }}</strong>
+                </div>
+            @endif
+            @if ($message = Session::get('Event'))
+                <div class="alert alert-primary alert-block">
+                    <button type="button" class="close" data-dismiss="alert">×</button>
+                    <strong>{{ $message }}</strong>
+                </div>
+            @endif
+        </div><!-- /.container-fluid -->
+    </section>
 
-        <!-- Main content -->
-        <section class="content">
-            <div class="container-fluid " style="width: 5500px">
-                <div class="row">
-                    <div class="col-md-3">
 
-                        <!-- Profile Image -->
-                        <div class="card card-primary card-outline">
-                            <div class="card-body box-profile" >
-                                <div class="text-center">
-                                    <img class="profile-user-img img-fluid img-circle"
-                                         src="{{asset('portal/dist/img/user4-128x128.jpg')}}"
-                                         alt="User profile picture">
-                                </div>
 
-                                <h3 class="profile-username text-center">{{Auth::user()->name}}</h3>
-                                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                    <h6 class="m-0 ml-4 font-weight-bold text-primary">Edit</h6>
-                                    <div class="dropdown no-arrow">
-                                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400 ml-5"></i>
-                                        </a>
-                                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-                                            <div class="dropdown-header">Photo Setting:</div>
-                                            <a class="dropdown-item" href="{{route('Avatar')}}">Change Your Photo</a>
-                                            <a class="dropdown-item" href="#">Change Your Password</a>
-                                            <a class="dropdown-item" href="#">Change Your Profile</a>
-                                            {{--                                        <div class="dropdown-divider"></div>--}}
-                                            {{--                                        <a class="dropdown-item" href="#">Something else here</a>--}}
-                                        </div>
-                                    </div>
-                                </div>
-                                <p class="text-muted text-center">Software Engineer</p>
-                            </div>
-                            <!-- /.card-body -->
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-8">
+
+                    <!-- Default Card Example -->
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            <h6 class="m-0 font-weight-bold text-primary">
+                                FullName:
+                            </h6>
+                        </div>
+                        <div class="card-body text-capitalize" >
+                            id: {{Auth::user()->id}} <br>
+                            Name: {{Auth::user()->name}}<br>
+                            Family: {{Auth::user()->name}}<br>
+                            Email: {{Auth::user()->email}}
                         </div>
                     </div>
+
+                    <!-- Basic Card Example -->
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">Contact Info</h6>
+                        </div>
+                        <div class="card-body text-capitalize">
+                            Email: {{Auth::user()->email}}<br>
+                            MobilePhone: ##########<br>
+                            office Phone: ##########<br>
+                        </div>
+                    </div>
+
+                    <!-- Collapsable Card Example -->
+                    <div class="card shadow mb-4 ">
+                        <!-- Card Header - Accordion -->
+                        <a href="#collapseCardExample" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="collapseCardExample">
+                            <h6 class="m-0 font-weight-bold text-primary">Task Calender</h6>
+                        </a>
+                        <!-- Card Content - Collapse -->
+                        <div class="collapse show" id="collapseCardExample">
+                            <div class="card card-body">
+                                <form method="post" action="#" id="Events">
+                                    @csrf
+                                    <input type="datetime-local" name="date" required>
+                                    <label for="title" >Title</label>
+                                    <input type="text" id="title" name="title" required>
+                                    <label for="event">Event</label>
+                                </form>
+                                <textarea rows="4" cols="50" name="comment" form="Events"></textarea>
+                                <input class="w-25 btn btn-primary p-2 m-2" type="submit" form="Events" required>
+                            </div>
+                        </div>
+
+                    </div>
+
                 </div>
-                <!-- /.row -->
-            </div><!-- /.container-fluid -->
 
+                <div class="card shadow mb-4 col-lg-3" style="height: 368px">
+                    <!-- Card Header - Dropdown -->
+                    <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                        <h6 class="m-0 font-weight-bold text-primary">Profile Photo</h6>
+                        <div class="dropdown no-arrow ml-5">
+                            <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
+                                <div class="dropdown-header">Photo Setting:</div>
+                                <a class="dropdown-item" href="#">Change your photo</a>
+                                <a class="dropdown-item" href="#">Delete</a>
 
-        </section>
-        <div class="card card-primary">
-            <div class="card-header">
-                <h3 class="card-title">About Me</h3>
-            </div>
-            <!-- /.card-header -->
-            <div class="card-body">
-                <strong><i class="fas fa-book mr-1"></i> Education</strong>
-
-                <p class="text-muted">
-                    B.S. in Computer Science from the University of Tennessee at Knoxville
-                </p>
-
-                <hr>
-
-                <strong><i class="fas fa-map-marker-alt mr-1"></i> Location</strong>
-
-                <p class="text-muted">Malibu, California</p>
-
-                <hr>
-
-                <strong><i class="fas fa-pencil-alt mr-1"></i> Skills</strong>
-
-                <p class="text-muted">
-                    <span class="tag tag-danger">UI Design</span>
-                    <span class="tag tag-success">Coding</span>
-                    <span class="tag tag-info">Javascript</span>
-                    <span class="tag tag-warning">PHP</span>
-                    <span class="tag tag-primary">Node.js</span>
-                </p>
-
-                <hr>
-
-                <strong><i class="far fa-file-alt mr-1"></i> Notes</strong>
-
-                <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fermentum enim neque.</p>
-            </div>
-            <!-- /.card-body -->
-        </div>
-        <!-- /.content -->
-    </div>
-    <!-- /.content-wrapper -->
-
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Card Body -->
+                    <div class="card-body">
+                        <img src="{{asset("portal/dist/img/avatar3.png")}}" alt="profil image" class="rounded mx-auto d-block" style="width: 200px; height: 266px">
+                        {{--                                    <img src="<?php $url = Storage::url(\Illuminate\Support\Facades\Auth::user()->img); echo asset("$url")?>" class="rounded mx-auto d-block" style="width: 200px; height: 266px">--}}
+                    </div>
+                </div>
+                <!-- /.col -->
 @endsection
